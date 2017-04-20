8a7581f21eb1d87807c18771aa3b3a00aa71891d
Ben <Ben@Precious>
2017-04-20 03:20:39 -0500

    Currently trying to populate JSON object from MySQL.  Maybe on page load, or even program start?

--- MIS320_Final/web/resources/scripts/AutoComplete.js
+++ MIS320_Final/web/resources/scripts/AutoComplete.js
@@ -3,18 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  *//*jslint  browser: true, white: true, plusplus: true */
-        /*global $, films */
+        /*global $, films*/
         $(function () {
             'use strict';
+            //var filmArray1 = 
             var filmArray = $.map(films, function (value, key) {
                 return {value: value, data: key};
             });
+            console.log(filmArray);
             // Initialize ajax autocomplete:
             $('#autocomplete').autocomplete({
                 lookup: filmArray,
                 lookupLimit: 10,
                 minChars: 2,
                 onSelect: function (suggestion) {
-                    $('#selction-ajax').html('You selected: ' + suggestion.value + ', ' + suggestion.data);
+                    $('#selection-ajax').html('You selected: ' + suggestion.value + ', ' + suggestion.data);
                 }});
         });
