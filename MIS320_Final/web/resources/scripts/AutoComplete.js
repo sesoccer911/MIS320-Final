edf1690e0afe465d6fbb69583d919424f08b1039
Ben <Ben@Precious>
2017-04-29 14:05:55 -0500

    Added actor, not worrying about that right now

--- MIS320_Final/web/resources/scripts/AutoComplete.js
+++ MIS320_Final/web/resources/scripts/AutoComplete.js
@@ -3,15 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  *//*jslint  browser: true, white: true, plusplus: true */
-        /*global $, films, Packages */
+        /*global $, films, Packages, Actors */
         $(function () {
             'use strict';
             var filmArray = $.map(films, function (value, key) {
                 return {value: value, data: key};
             });
+            var actorArray = $.map(Actors, function (value, key) {
+                return {value: value, data: key};
+            });
             // Initialize ajax autocomplete:
             $('#autocomplete').autocomplete({
                 lookup: filmArray,
+                lookupLimit: 20,
+                minChars: 2,
+                onSelect: function (suggestion) {
+                    $('#selection-ajax').value(suggestion.data);
+                }});
+            $('#actor-autocomplete').autocomplete({
+                lookup: actorArray,
                 lookupLimit: 20,
                 minChars: 2,
                 onSelect: function (suggestion) {
